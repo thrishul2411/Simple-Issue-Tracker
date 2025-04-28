b6e8239922c3c47f6d7dde86aa26fefdc6140274
thrishul2411 <polisettythrishul@gmail.com>
2025-04-28 07:54:17 +0530

    project creation

---
+++ src/main/java/com/sit/simpleissuetracker/SimpleIssueTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sit.simpleissuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleIssueTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleIssueTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sit/simpleissuetracker/SimpleIssueTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sit.simpleissuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleIssueTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
